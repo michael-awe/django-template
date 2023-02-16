5068814211703ef9fd5afb50f2ca0db30071d873
Michael Awe <ionicawe@gmail.com>
2023-02-16 14:32:26 +0200

    Add login and signup links to home page. Minor changes to dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,6 +6,6 @@ def index(request):
 
     #if user is not logged in, redirect to home page
     if not request.user.is_authenticated:
-        return redirect('landing_page:index')
+        return redirect('accounts:login')
     
     return render(request, 'dashboard/index.html')
