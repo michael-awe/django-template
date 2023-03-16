dddbdf9a589854a28e89ea5fb80b121876958cf3
Michael Awe <ionicawe@gmail.com>
2023-03-16 20:47:40 +0200

    Add rename management command

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ python manage.py makemigrations && python manage.py migrate
 #Renames project
 echo "Enter a new project name: (ensure name is allowed by Django before pressing enter)"
 read project_name
-sudo python3 rename.py 'djangotemplate' $project_name
+python manage.py rename 'djangotemplate' $project_name
 
 #Run server
 python manage.py runserver
