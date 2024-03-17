7f43fb6652302b29b06e2530b0d10cb1c63a77df
jonathanh <jonathanhouge@email.arizona.edu>
2024-03-17 12:39:42 -0700

    fixed registration form, whoops!

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,4 +5,4 @@ from .models import User
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["username", "email", "password"]
+        fields = ["username", "email", "password1", "password2"]
