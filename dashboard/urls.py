f3e424d19c072271e6d98bb3db109f4f2dd7c328
Cooper Harris <cwharris365@gmail.com>
2024-03-17 22:45:17 -0700

    Added views for getting all movies and one movie by id. Added routes for creating, updating, and deleting a movie

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'dashboard'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('movies/', views.get_all_movies, name='get_all_movies'),
+    path('movies/<int:movie_id>/', views.get_movie, name='get_movie'),
+    path('movies/create/', views.create_movie, name='create_movie'),
+    path('movies/<int:movie_id>/delete/', views.delete_movie, name='delete_movie'),
+    path('movies/<int:movie_id>/update/', views.update_movie, name='update_movie'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+from recommendations.models import Movie
+
 
 # Create your views here.
+def is_admin(user):
+    return user.is_superuser
+
 def index(request):
 
     #if user is not logged in, redirect to home page
@@ -9,3 +15,31 @@ def index(request):
         return redirect('accounts:login')
     
     return render(request, 'dashboard/index.html')
+
+@login_required
+def get_all_movies(request):
+    all_movies = Movie.objects.all()
+   
+    return render(request, 'dashboard/movies_list.html', {'movies': all_movies})
+
+@login_required
+def get_movie(request, movie_id):
+    try:
+        movie = Movie.objects.get(pk=movie_id)
+    except:
+        # Movie id doesn't exist
+        return render(request, 'dashboard/404.html')
+    
+    return render(request, 'dashboard/movie_details.html', {'movie': movie})
+
+@user_passes_test(is_admin)
+def create_movie(request):
+    pass
+
+@user_passes_test(is_admin)
+def delete_movie(request):
+    pass
+
+@user_passes_test(is_admin)
+def update_movie(request):
+    pass
