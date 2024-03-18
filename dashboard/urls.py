78235e2f126b529f3809295c7b98d2eaf1bd6a5c
Cooper Harris <cwharris365@gmail.com>
2024-03-18 09:55:33 -0700

    Addressing requested changes and updated dashboard/base.html to include a block title

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path, include
 from . import views
 
-app_name = 'dashboard'
+app_name = "dashboard"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('movies/', views.get_all_movies, name='get_all_movies'),
-    path('movies/<int:movie_id>/', views.get_movie, name='get_movie'),
-    path('movies/create/', views.create_movie, name='create_movie'),
-    path('movies/<int:movie_id>/delete/', views.delete_movie, name='delete_movie'),
-    path('movies/<int:movie_id>/update/', views.update_movie, name='update_movie'),
+    path("", views.index, name="index"),
+    path("movies/create/", views.create_movie, name="create_movie"),
+    path("movies/<int:movie_id>/delete/", views.delete_movie, name="delete_movie"),
+    path("movies/<int:movie_id>/update/", views.update_movie, name="update_movie"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -8,37 +8,41 @@ from recommendations.models import Movie
 def is_admin(user):
     return user.is_superuser
 
+
 def index(request):
 
-    #if user is not logged in, redirect to home page
+    # if user is not logged in, redirect to home page
     if not request.user.is_authenticated:
-        return redirect('accounts:login')
-    
-    return render(request, 'dashboard/index.html')
+        return redirect("accounts:login")
 
-@login_required
+    return render(request, "dashboard/index.html")
+
+
 def get_all_movies(request):
     all_movies = Movie.objects.all()
-   
-    return render(request, 'dashboard/movies_list.html', {'movies': all_movies})
 
-@login_required
+    return render(request, "dashboard/movies_list.html", {"movies": all_movies})
+
+
 def get_movie(request, movie_id):
     try:
         movie = Movie.objects.get(pk=movie_id)
     except:
         # Movie id doesn't exist
-        return render(request, 'dashboard/404.html')
-    
-    return render(request, 'dashboard/movie_details.html', {'movie': movie})
+        return render(request, "dashboard/404.html")
+
+    return render(request, "dashboard/movie_details.html", {"movie": movie})
+
 
 @user_passes_test(is_admin)
 def create_movie(request):
     pass
 
+
 @user_passes_test(is_admin)
 def delete_movie(request):
     pass
+
 
 @user_passes_test(is_admin)
 def update_movie(request):
