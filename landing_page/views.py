1188fddc650064197d89c46166659ee20b765b75
farrahirodriguez <102585852+farrahirodriguez@users.noreply.github.com>
2024-03-19 20:29:52 -0700

    Homepage and About page
    Created homepage (index.html) and about page (about.html). Both pages use base.html which is where the header and footer can be found. base.css can be used to style the header and footer as well as any common elements between the homepage and about page.

--- landing_page/views.py
+++ landing_page/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Initial landing page view.
 def index(request):
     return render(request, 'landing_page/index.html')
+    return render(request, "landing_page/about.html")
 
 
 #Add other views here
