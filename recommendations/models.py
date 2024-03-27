d5bd2e31383b9b3d0f5bbc92de4b1589ebbeed65
Cooper Harris <cwharris365@gmail.com>
2024-03-26 19:19:01 -0700

    Removing fields from movie model

--- recommendations/models.py
+++ recommendations/models.py
@@ -6,6 +6,3 @@ class Movie(models.Model):
     name = models.CharField(max_length=30, unique=False)
     year = models.PositiveIntegerField()
     director = models.CharField(max_length=60, unique=False)
-    watched = models.BooleanField(default=False)
-    recommend = models.BooleanField(default=True)
-    rewatch = models.BooleanField(default=False)
