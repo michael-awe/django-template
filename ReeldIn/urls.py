477788d4374e8f71f7f96cfd7c090b2e8eba9a03
jonathanh <jonathanhouge@email.arizona.edu>
2024-03-26 20:32:13 -0700

    redirecting to recommendation page successfully

--- ReeldIn/urls.py
+++ ReeldIn/urls.py
@@ -22,5 +22,8 @@ urlpatterns = [
     path("", include("landing_page.urls", namespace="landing_page")),
     path("dashboard/", include("dashboard.urls", namespace="dashboard")),
     path("accounts/", include("accounts.urls", namespace="accounts")),
+    path(
+        "recommendations/", include("recommendations.urls", namespace="recommendations")
+    ),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
--- recommendations/views.py
+++ recommendations/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse
 
 
 def recommend_view(request):
+    print(request)
 
-    if request.method == "POST":
-        answers = request.POST["answers"]
+
+def index(request):
+    return render(request, "recommendations/index.html")
