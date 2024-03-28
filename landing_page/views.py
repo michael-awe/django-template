16be84980644c9d98723f2f8347c6fa9ec6d245f
ElizabethODD <elizabethwuhong9@arizona.edu>
2024-03-28 14:42:20 -0700

    movie.html and css

--- landing_page/views.py
+++ landing_page/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Initial landing page view.
 def index(request):
-    return render(request, "landing_page/index.html")
+    return render(request, "landing_page/movie.html")
 
 
 # Add other views here
