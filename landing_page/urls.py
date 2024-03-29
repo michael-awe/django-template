e7666d61bcd18a56b38c1c28e3aa0462ec1b7971
farrahirodriguez <102585852+farrahirodriguez@users.noreply.github.com>
2024-03-28 23:24:32 -0700

    Login, Contact page, 404 styling, and fixes
    - Deleted default files that came with the repo - auth.css & reset.css
    
    New Files include:
    - contact.html & contact.css: feedback/contact form
       - I found this site that will allow us to have submissions be sent directly to an email but it won't work yet as the site isn't up
    
    - login.html, login.css, and login.js: for the signup/login page
    - 404.css: styling for the error page

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 app_name = "landing_page"
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+]
--- landing_page/views.py
+++ landing_page/views.py
@@ -6,4 +6,12 @@ def index(request):
     return render(request, "landing_page/index.html")
 
 
+def about(request):
+    return render(request, "landing_page/about.html")
+
+
+def contact(request):
+    return render(request, "landing_page/contact.html")
+
+
 # Add other views here
