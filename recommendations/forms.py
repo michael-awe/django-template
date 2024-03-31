531c8b60bd45d6a42435c98aca75d080a563e369
jonathanh <jonathanhouge@email.arizona.edu>
2024-03-31 13:52:55 -0700

    choices.py, expansion of rec/models.py

--- recommendations/forms.py
+++ recommendations/forms.py
@@ -1,27 +1,9 @@
 from django import forms
+from .choices import *
 
 
 class GenreForm(forms.Form):
-    OPTIONS = (
-        ("Action", "Action"),
-        ("Adventure", "Adventure"),
-        ("Animation", "Animation"),
-        ("Comedy", "Comedy"),
-        ("Crime", "Crime"),
-        ("Documentary", "Documentary"),
-        ("Drama", "Drama"),
-        ("Family", "Family"),
-        ("Fantasy", "Fantasy"),
-        ("History", "History"),
-        ("Horror", "Horror"),
-        ("Music", "Music"),
-        ("Mystery", "Mystery"),
-        ("Romance", "Romance"),
-        ("Sci-Fi", "Science Fiction"),
-        ("Thriller", "Thriller"),
-        ("War", "War"),
-        ("Western", "Western"),
-    )
+    OPTIONS = GENRES
     Genres = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple, choices=OPTIONS
     )
--- recommendations/models.py
+++ recommendations/models.py
@@ -1,8 +1,23 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from .choices import *
 
 
-# Create your models here.
 class Movie(models.Model):
     name = models.CharField(max_length=30, unique=False)
+    genre = models.CharField(max_length=13, unique=False, choices=GENRES)
     year = models.PositiveIntegerField()
-    director = models.CharField(max_length=60, unique=False)
+    runtime = models.PositiveIntegerField()
+    language = models.CharField(max_length=30, unique=False, choices=LANGUAGES)
+
+    # people
+    top_cast = ArrayField(models.CharField(max_length=60, unique=False))
+    directors = ArrayField(models.CharField(max_length=60, unique=False))
+    writers = ArrayField(models.CharField(max_length=60, unique=False))
+    cinematographer = ArrayField(models.CharField(max_length=60, unique=False))
+    composer = ArrayField(models.CharField(max_length=60, unique=False))
+
+
+class Recommendation(models.Model):
+    genre = models.CharField(max_length=13, unique=False, choices=GENRES)
+    year_span = models.PositiveIntegerField(choices=YEAR_SPANS)
