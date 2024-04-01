806547708d2f074e1386909ed3c86f7e1f22fd8a
jonathanh <jonathanhouge@email.arizona.edu>
2024-03-31 17:18:56 -0700

    triggers and furthered fields

--- recommendations/models.py
+++ recommendations/models.py
@@ -4,20 +4,25 @@ from .choices import *
 
 
 class Movie(models.Model):
-    name = models.CharField(max_length=30, unique=False)
-    genre = models.CharField(max_length=13, unique=False, choices=GENRES)
+    name = models.CharField(max_length=30)
+    genre = models.CharField(max_length=13, choices=GENRES)
     year = models.PositiveIntegerField()
     runtime = models.PositiveIntegerField()
-    language = models.CharField(max_length=30, unique=False, choices=LANGUAGES)
+    language = models.CharField(max_length=30, choices=LANGUAGES)
 
     # people
-    top_cast = ArrayField(models.CharField(max_length=60, unique=False))
-    directors = ArrayField(models.CharField(max_length=60, unique=False))
-    writers = ArrayField(models.CharField(max_length=60, unique=False))
-    cinematographer = ArrayField(models.CharField(max_length=60, unique=False))
-    composer = ArrayField(models.CharField(max_length=60, unique=False))
+    top_cast = ArrayField(models.CharField(max_length=60))
+    directors = ArrayField(models.CharField(max_length=60))
+    writers = ArrayField(models.CharField(max_length=60))
+    cinematographer = ArrayField(models.CharField(max_length=60))
+    composer = ArrayField(models.CharField(max_length=60))
+
+    uh = ArrayField(models.CharField(max_length=60, choices=TRIGGERS))
+    watch_providers = ArrayField(models.CharField(max_length=60))
+    tmdb_keywords = ArrayField(models.CharField(max_length=60))
+    tmdb_id = models.PositiveIntegerField()
 
 
 class Recommendation(models.Model):
-    genre = models.CharField(max_length=13, unique=False, choices=GENRES)
+    genre = models.CharField(max_length=13, , choices=GENRES)
     year_span = models.PositiveIntegerField(choices=YEAR_SPANS)
