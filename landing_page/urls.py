68ddb6322254fd8f65458fa7054ffdfd09e53703
ElizabethODD <elizabethwuhong9@arizona.edu>
2024-04-01 19:40:25 -0700

    fixed conflicts

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 app_name = "landing_page"
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+    path("search/movies", views.search_movies, name="search_movies")
+]
