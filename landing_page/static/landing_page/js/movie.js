68ef36a2deae09b7fe5c2e7c743a3f60d66660c0
ElizabethODD <elizabethwuhong9@arizona.edu>
2024-04-05 22:09:12 -0700

    added movie.js file

---
+++ landing_page/static/landing_page/js/movie.js
@@ -0,0 +1,14 @@
+
+function movieOverview(){
+    document.getElementById("movie_summary").innerHTML = `Description of <b>Batman</b> Movie.
+    <div> More Information Here</div>`;
+}
+function movieDetails(){
+    document.getElementById("movie_summary").innerHTML = "Movie Genre";
+}
+function movieWatching(){
+    document.getElementById("movie_summary").innerHTML = "Movie Streaming/Buy/Rent";
+}
+function movieOptions(){
+    document.getElementById("movie_summary").innerHTML = "Movie Comment/Rating/etc.";
+}
