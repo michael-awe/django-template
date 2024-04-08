7a6fae8ce2da16972e8e2362ec950f04e2e9ce5b
ElizabethODD <elizabethwuhong9@arizona.edu>
2024-04-07 18:01:52 -0700

    updated movie.js

--- landing_page/static/landing_page/js/movie.js
+++ landing_page/static/landing_page/js/movie.js
@@ -1,14 +1,44 @@
 
+// Buttons for the movie page's 
+// context change depending on button clicked, testing visuals
 function movieOverview(){
-    document.getElementById("movie_summary").innerHTML = `Description of <b>Batman</b> Movie.
-    <div> More Information Here</div>`;
+    document.getElementById("movie_summary").innerHTML = 
+    `Description of <b>Dark Knight</b> Movie: Movie Summary/Plot
+    `;
 }
 function movieDetails(){
-    document.getElementById("movie_summary").innerHTML = "Movie Genre";
+    document.getElementById("movie_summary").innerHTML = 
+    `<div>Director(s):
+    </div>
+    <div> 
+    Writer(s):
+    </div>
+    <div>
+    Star(s):
+    </div>
+    <div>
+    Genre(s):
+    </div>
+    `;
 }
+// movie available to watch 
 function movieWatching(){
-    document.getElementById("movie_summary").innerHTML = "Movie Streaming/Buy/Rent";
+    document.getElementById("movie_summary").innerHTML = 
+    `<div>Streaming: 
+    </div>
+    <div>Buy: 
+    </div>
+    <div>Rent: 
+    </div>
+    `;
 }
 function movieOptions(){
-    document.getElementById("movie_summary").innerHTML = "Movie Comment/Rating/etc.";
+    document.getElementById("movie_summary").innerHTML = 
+    `<div>
+    Movie Comment(s): 
+    </div>
+    <div>
+    Movie Rating(s):
+    </div>
+    `;
 }
